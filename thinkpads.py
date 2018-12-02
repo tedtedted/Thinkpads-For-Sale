f526efbad5623c5835dbbc5e1ff4b24440b4ddb8
Ted Redington <tedredington@gmail.com>
2018-12-02 12:11:46 -0800

    added reddit and cl functions

---
+++ thinkpads.py
@@ -0,0 +1,29 @@
+from craigslist import CraigslistForSale
+import praw
+
+import settings
+
+def craigslist_thinkpads():
+
+    cl = CraigslistForSale(filters={'query':'T480'})
+
+    for result in cl.get_results(sort_by='newest'):
+        if 'T480' in result['name']:
+            print(result)
+
+
+def r_thinkpads():
+
+    reddit = praw.Reddit(client_id=settings.REDDIT_CLIENT_ID,
+                     client_secret=settings.REDDIT_CLIENT_SECRET,
+                     password=settings.REDDIT_PASSWORD,
+                     user_agent=settings.REDDIT_USER_AGENT,
+                     username=settings.REDDIT_USERNAME)
+
+    subreddit = reddit.subreddit('thinkpadsforsale')
+
+    for post in subreddit.new():
+        if 'T480' in post.title:
+            print(post.title)
+
+r_thinkpads()
