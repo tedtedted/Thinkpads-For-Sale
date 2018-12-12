39490523706d1de37fa2ed4fc6f6e03fba2e8a93
Ted Redington <tedredington@gmail.com>
2018-12-11 20:51:33 -0800

    updating regex

--- thinkpads.py
+++ thinkpads.py
@@ -5,19 +5,21 @@ import praw
 
 import settings
 
+MODEL = 'T480'
+
 def craigslist_thinkpads():
 
-    cl = CraigslistForSale(filters={'query':'T480'})
+    cl = CraigslistForSale(filters={'query':MODEL})
 
     for result in cl.get_results(sort_by='newest'):
-        if 'T480' in result['name']:
+        if MODEL in result['name']:
             print(result)
 
 
 def r_thinkpads():
 
     #Still not working 
-    regex = r'\[H\]*(?:(?!\[W\]).)*'
+    regex = re.compile(r'\[H\].*(T480).* (.*?)\[W\]')
 
     reddit = praw.Reddit(client_id=settings.REDDIT_CLIENT_ID,
                      client_secret=settings.REDDIT_CLIENT_SECRET,
@@ -29,8 +31,11 @@ def r_thinkpads():
 
 
     for post in subreddit.new():
-      
-        if re.match(regex, post.title):
+        
+        if regex.search(post.title):
             print(post.title)
 
+print('Checking Craigslist')
+craigslist_thinkpads()
+print('Checking r/Thinkpadsforsale')
 r_thinkpads()
