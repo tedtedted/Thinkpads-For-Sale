455e68a1fe492a82ec0073434cf48f867d700e36
Ted Redington <tedredington@gmail.com>
2020-02-29 14:48:17 -0800

    formatted with black

--- thinkpads.py
+++ thinkpads.py
@@ -5,42 +5,44 @@ import praw
 
 import settings
 
-MODEL = 'T480'
+MODEL = "T480"
 
 
 def main():
-    print('Checking Craigslist')
+    print("Checking Craigslist")
     craigslist_thinkpads()
 
-    print('Checking r/Thinkpadsforsale')
+    print("Checking r/Thinkpadsforsale")
     r_thinkpads()
 
 
 def craigslist_thinkpads():
 
-    cl = CraigslistForSale(filters={'query':MODEL})
+    cl = CraigslistForSale(filters={"query": MODEL})
 
-    for result in cl.get_results(sort_by='newest'):
-        if MODEL in result['name']:
+    for result in cl.get_results(sort_by="newest"):
+        if MODEL in result["name"]:
             print(result)
 
 
 def r_thinkpads():
 
-    regex = re.compile(rf'\[H\].*({MODEL}).* (.*?)\[W\]')
+    regex = re.compile(rf"\[H\].*({MODEL}).* (.*?)\[W\]")
 
-    reddit = praw.Reddit(client_id=settings.REDDIT_CLIENT_ID,
-                     client_secret=settings.REDDIT_CLIENT_SECRET,
-                     password=settings.REDDIT_PASSWORD,
-                     user_agent=settings.REDDIT_USER_AGENT,
-                     username=settings.REDDIT_USERNAME)
+    reddit = praw.Reddit(
+        client_id=settings.REDDIT_CLIENT_ID,
+        client_secret=settings.REDDIT_CLIENT_SECRET,
+        password=settings.REDDIT_PASSWORD,
+        user_agent=settings.REDDIT_USER_AGENT,
+        username=settings.REDDIT_USERNAME,
+    )
 
-    subreddit = reddit.subreddit('thinkpadsforsale')
+    subreddit = reddit.subreddit("thinkpadsforsale")
 
     for post in subreddit.new():
         if regex.search(post.title):
             print(post.title)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
